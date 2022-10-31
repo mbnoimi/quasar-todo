189ec0af14e4cc0d8a0b9c281407f4f6a00a3e68
mbnoimi <mbnoimi@gmail.com>
2022-10-31 12:38:16 +0300

    #3 - Enabling mdi icons - Main todo section

--- quasar.config.js
+++ quasar.config.js
@@ -42,7 +42,7 @@ module.exports = configure(function (/* ctx */) {
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
       // 'ionicons-v4',
-      // 'mdi-v5',
+      'mdi-v5',
       // 'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
@@ -56,7 +56,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
       },
 
@@ -101,6 +101,7 @@ module.exports = configure(function (/* ctx */) {
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
+      iconSet: 'mdi-v5', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
 
       // For special cases outside of where the auto-import strategy can have an impact
@@ -133,7 +134,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
+      // will mess up SSR
 
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
@@ -144,7 +145,7 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,36 @@
 <template>
-  <q-page padding>
-    <q-select
-      v-model="selected"
-      label="numbers"
-      filled
-      :options="['one', 'two', 'three']"
-    ></q-select>
+  <q-page class="row">
+    <q-card class="col-xs-9">
+      <q-toolbar>
+        <q-input
+          dense
+          placeholder="Search"
+          borderless
+          class="full-width"
+          :model-value="search"
+        >
+          <template #append>
+            <q-icon name="mdi-magnify" />
+          </template>
+        </q-input>
+      </q-toolbar>
+      <q-list bordered>
+        <q-item>
+          <q-item-section side>
+            <q-checkbox :model-value="true" />
+          </q-item-section>
+          <q-item-section> Go shopping </q-item-section>
+          <q-item-section side>
+            <q-btn size="sm" round flat icon="mdi-delete"></q-btn>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const selected = ref('');
+const search = ref('');
 </script>
