515c4784023ade96a8f2c3383664c67350414bc8
mbnoimi <mbnoimi@gmail.com>
2022-11-01 10:52:18 +0300

    #8 Adding/Using plugins

--- src/components/CreateTodoButton.vue
+++ src/components/CreateTodoButton.vue
@@ -1,7 +1,32 @@
 <template>
-  <q-btn />
+  <q-btn @click="clickTodoButton" />
 </template>
 
 <script setup lang="ts">
-//
+import { useQuasar } from 'quasar';
+const $q = useQuasar();
+function clickTodoButton() {
+  $q.dialog({
+    title: 'Create Todo',
+    message: 'Input your todo content',
+    prompt: {
+      model: '',
+      type: 'text', // optional
+    },
+    cancel: true,
+    persistent: true,
+  })
+    .onOk(createTodo)
+    .onCancel(() => {
+      // console.log('>>>> Cancel')
+    });
+}
+
+function createTodo() {
+  $q.notify({
+    message: 'Todo Created!',
+    icon: 'mdi-check',
+    color: 'positive',
+  });
+}
 </script>
